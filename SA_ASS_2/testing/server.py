f146b3aeae1a58045daad965c13603342f353185
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-09-17 16:12:55 +0300

    initial commit

---
+++ SA_ASS_2/testing/server.py
@@ -0,0 +1,70 @@
+import socket
+import threading
+from threading import Thread
+import argparse
+# server's IP address
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 10000
+separator_token = "<SEP>"
+client_sockets = set()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-ipp", "--ip_port", required=True)
+args = parser.parse_args()
+print(f'Hi {args.ip_port}, Welcome')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((SERVER_HOST, SERVER_PORT))
+
+s.listen(100)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+lock = threading.Lock()
+
+flag = True
+lst = []
+lst.append("Previous messages\n")
+end_of_lst = "End of previous messages\n"
+
+def listen_for_client(cs):
+    """
+    This function keep listening for a message from `cs` socket
+    Whenever a message is received, broadcast it to all other connected clients
+    """
+    with lock:
+        for item in lst:
+            cs.send(item.encode())
+        cs.send(end_of_lst.encode())
+
+    while True:
+        try:
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            msg = msg.replace(separator_token, ":")
+            with lock:
+                lst.append(msg + "\n")
+            msg_parts = msg.split(":")
+            print(f"[*] Received: {msg}")
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
+
+try:
+    while flag:
+        client_socket, client_address = s.accept()
+        print(f"[+] {client_address} connected.")
+        client_sockets.add(client_socket)
+        t = Thread(target=listen_for_client, args=(client_socket,), daemon=True)
+        t.start()
+except KeyboardInterrupt:
+    print(f"[!] Keyboard Interrupt")
+
+for cs in client_sockets:
+    cs.close()
+s.close()
+
